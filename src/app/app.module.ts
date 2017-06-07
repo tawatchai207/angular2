fec6ea4c7b7118f208418c4ca399e7d6c3d1e9a0
tawatchai207 <tipooy@gmail.com>
2017-06-07 09:18:53 +0700

    learning first angular2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { HeroService } from './hero.service';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path: '',
+  component: AboutComponent
+}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeroComponent,
+    HeroListComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+    //RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
